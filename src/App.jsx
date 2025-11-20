3bd827a3a8c6252f5b594d6d12a9ee2fb2f9909f
jocelynngo-debug <jocelyn.ngo@jajags.com>
2025-11-20 08:49:30 -0700

    Checkpoint 1: Initial React project setup with Mantine

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './components/Header'
+import './App.css'
+
+
+function App() {
+ return (
+   <div className="App">
+     <Header />
+     <p>Welcome to your React-powered class organizer!</p>
+   </div>
+ )
+}
+
+
+export default App
